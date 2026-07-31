983d3d3055e8f3165ff37dda33d5a43c1e1d6f4e
Marouane Bounou <maroinbounou.123@gmail.com>
2026-07-31 10:31:56 +0100

    inisilize backend

---
+++ server/src/main/java/com/example/flowos/FlowosApplication.java
@@ -0,0 +1,13 @@
+package com.example.flowos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowosApplication.class, args);
+	}
+
+}
